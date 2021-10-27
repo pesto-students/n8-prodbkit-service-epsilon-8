befb642988ac9418d1d624284a99931049298bdb
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2021-10-28 00:32:22 +0530

    feat: add dashboard stats api endpoint - controller file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 export class LoginDTO {
   @ApiProperty()
   id_token: string;
@@ -10,11 +11,20 @@ export class LoginDTO {
 @ApiTags('auth')
 @Controller('/api/auth')
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private appService: AppService,
+  ) {}
 
   @ApiBody({ type: LoginDTO })
   @Post('login')
   async login(@Body('id_token') id_token: string) {
     return this.authService.loginWithIdToken(id_token);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('dashboard-stats')
+  getDashboardStats() {
+    return this.appService.getDashboardStats();
   }
 }
